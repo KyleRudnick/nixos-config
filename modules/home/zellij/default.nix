80835b30d1edc432752275ffe2eef529071d7694
Kyle Rudnick <rudnick.kyle@gmail.com>
2025-09-10 01:36:48 +0200

    Regression for merge

--- modules/home/zellij/default.nix
+++ modules/home/zellij/default.nix
@@ -11,8 +11,8 @@
   };
 
   home.file.".config/zellij/config.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/zellij/config.kdl";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/NixOS/dotfiles/zellij/config.kdl";
 
   home.file.".config/zellij/layouts".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/zellij/layouts";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/NixOS/dotfiles/zellij/layouts";
 }
