11b30800383d049c2967a01493f8a6ef9fd50b19
Kyle Rudnick <rudnick.kyle@gmail.com>
2025-11-10 12:58:05 +0100

    Readded Licensing to comply with MIT License

---
+++ README.md
@@ -0,0 +1,2 @@
+My config files.
+The nixos config is based on ZaneyOS (License in LICENSE.ZANEY.md).
--- hosts/default/host-packages.nix
+++
@@ -1,6 +0,0 @@
-{pkgs, pkgs-unstable, ...}: {
-  environment.systemPackages = with pkgs; [
-    discord
-    vim
-  ];
-}
